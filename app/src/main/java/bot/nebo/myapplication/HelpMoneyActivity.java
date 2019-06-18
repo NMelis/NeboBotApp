0dcded24fa5ff81dd0b61641118bc0e5612be301
Melis <melis.zhoroev@gmail.com>
2019-06-19 06:19:10 +0700

    Loggin new pages to Crashlytics

--- app/src/main/java/bot/nebo/myapplication/HelpMoneyActivity.java
+++ app/src/main/java/bot/nebo/myapplication/HelpMoneyActivity.java
@@ -9,21 +9,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.crashlytics.android.Crashlytics;
+
+import bot.nebo.myapplication.helper.Helper;
+import io.fabric.sdk.android.Fabric;
+
 public class HelpMoneyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_money);
+        if (!MainActivity.isDev) Fabric.with(this, new Crashlytics());
+        Helper.log("Open Help Project ->");
 
     }
 
     public void shareLink(View view){
+        Helper.log("Open Help Project -> Try shared Project");
         Intent myIntent = new Intent(Intent.ACTION_SEND);
         myIntent.setType("text/plain");
         String shareBody = "Бот для Небоскребов (nebo.mobi)";
         String shareSub = "Бот android приложение для игры Небоскребы (nebo.mobi): " +
-                                "Подробнее https://blog.nebolife.ru/2019/02/nebo-bot-android.html";
+                                "Подробнее https://blog.nebolife.ru/2019/02/nebo-bot-android.html" +
+                                " " +
+                                "Группа в вк: https://vk.com/club139649001";
         myIntent.putExtra(Intent.EXTRA_SUBJECT, shareBody);
         myIntent.putExtra(Intent.EXTRA_TEXT, shareSub);
         startActivity(Intent.createChooser(myIntent, "Рассказать друзьям"));
--- app/src/main/java/bot/nebo/myapplication/MoneySponsorListActivity.java
+++ app/src/main/java/bot/nebo/myapplication/MoneySponsorListActivity.java
@@ -8,32 +8,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.crashlytics.android.Crashlytics;
+
+import bot.nebo.myapplication.helper.Helper;
+import io.fabric.sdk.android.Fabric;
+
 public class MoneySponsorListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_money_sponsor_list);
+        if (!MainActivity.isDev) Fabric.with(this, new Crashlytics());
+        Helper.log("Open Help Money page");
     }
 
     public void Yandex(View view){
+        Helper.log("Open Help -> Yandex");
         Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(VKApplication.urlYandexMoney));
         startActivity(browserIntent);
     }
 
     public void YandexNumber(View view){
+        Helper.log("Open Help -> YandexNumber");
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         ClipData clip = ClipData.newPlainText("Скопирован Yandex номер: ", VKApplication.urlWebMoney);
         clipboard.setPrimaryClip(clip);
     }
 
     public void WebMoney(View view){
+        Helper.log("Open Help -> WebMoney");
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         ClipData clip = ClipData.newPlainText("Скопирован WebMoney номер, wmr:", VKApplication.urlWebMoney);
         clipboard.setPrimaryClip(clip);
     }
 
     public void Qiwi(View view){
+        Helper.log("Open Help -> Qiwi");
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         ClipData clip = ClipData.newPlainText("Скопирован Qiwi номер телефон:", VKApplication.urlQiwi);
         clipboard.setPrimaryClip(clip);
